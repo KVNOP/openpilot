5677f061c9ddca81408cf16e68c5d5436a1fb3bf
Jason Wen <haibin.wen3@gmail.com>
2022-03-29 18:10:48 -0400

    Non SCC maybe? - 19

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -222,8 +222,8 @@ static int hyundai_rx_hook(CANPacket_t *to_push) {
       acc_main_on_prev = acc_main_on;
     }
 
-    if (addr == 608 && HYUNDAI_SCC_BUS == -1) {
-      bool acc_main_on = (GET_BYTES_04(to_push) >> 25 & 0x1); // ACC main_on signal
+    if (addr == 1265 && HYUNDAI_SCC_BUS == -1) {
+      bool acc_main_on = (GET_BYTES_04(to_push) >> 3 & 0x1); // ACC main_on signal
       if (acc_main_on && !acc_main_on_prev)
       {
         controls_allowed = 1;
@@ -231,8 +231,8 @@ static int hyundai_rx_hook(CANPacket_t *to_push) {
       acc_main_on_prev = acc_main_on;
     }
 
-    if (addr == 608 && HYUNDAI_SCC_BUS == -1) {
-      bool acc_main_on = (GET_BYTES_04(to_push) >> 25 & 0x1); // ACC main_on signal
+    if (addr == 1265 && HYUNDAI_SCC_BUS == -1) {
+      bool acc_main_on = (GET_BYTES_04(to_push) >> 3 & 0x1); // ACC main_on signal
       if (acc_main_on_prev != acc_main_on)
       {
         disengageFromBrakes = false;
